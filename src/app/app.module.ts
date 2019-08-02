59f5b0528d91b4c7a368911ce4a45e6536e4990f
John Daligault <johndaligault@gmail.com>
2019-08-02 21:23:17 +0200

    adding grid system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule}                from './app-routing.module';
+import {AppComponent}                    from './app.component';
+import {BrowserAnimationsModule}         from '@angular/platform-browser/animations';
+import {MatCardModule, MatToolbarModule} from '@angular/material';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule(
+  {
+    declarations: [
+      AppComponent
+    ],
+    imports:      [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatCardModule,
+      MatToolbarModule
+    ],
+    providers:    [],
+    bootstrap:    [AppComponent]
+  }
+)
+export class AppModule {
+}
