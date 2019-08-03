15ffb9b47798ae9d8c8d9c465a457cb04c2db4b7
John Daligault <johndaligault@gmail.com>
2019-08-03 12:42:29 +0200

    adding vertical stepper for overview

--- src/app/pages/mood-overview/mood-overview.component.ts
+++ src/app/pages/mood-overview/mood-overview.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit}      from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-mood-overview',
-  templateUrl: './mood-overview.component.html',
-  styleUrls: ['./mood-overview.component.scss']
-})
+             selector:    'app-mood-overview',
+             templateUrl: './mood-overview.component.html',
+             styleUrls:   ['./mood-overview.component.scss']
+           })
 export class MoodOverviewComponent implements OnInit {
 
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
+    this.firstFormGroup  = this.formBuilder.group(
+      {
+        firstCtrl: ['']
+      }
+    );
+    this.secondFormGroup = this.formBuilder.group(
+      {
+        secondCtrl: ['']
+      }
+    );
   }
 
 }
