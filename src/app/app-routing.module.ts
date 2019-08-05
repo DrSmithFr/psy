51174849e389ef6f23cf538c64d79895de34c5e8
John Daligault <johndaligault@gmail.com>
2019-08-05 12:23:36 +0200

    adding clean pwa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,39 @@
-import { NgModule }               from '@angular/core';
-import { Routes, RouterModule }   from '@angular/router';
+import {NgModule}                 from '@angular/core';
+import {Routes, RouterModule}     from '@angular/router';
 import {MoodOverviewComponent}    from './pages/mood-overview/mood-overview.component';
 import {DashboardComponent}       from './pages/dashboard/dashboard.component';
 import {OverviewHistoryComponent} from './pages/overview-history/overview-history.component';
 import {SettingsComponent}        from './pages/settings/settings.component';
 
 const routes: Routes = [
-  {path: 'overview', component: MoodOverviewComponent},
-  {path: 'overview/history', component: OverviewHistoryComponent},
-  {path: 'settings', component: SettingsComponent},
-  {path: '', component: DashboardComponent},
+  {
+    path:      'mood/overview',
+    component: MoodOverviewComponent
+  },
+  {
+    path:      'mood/history',
+    component: OverviewHistoryComponent
+  },
+  {
+    path:      'mood',
+    component: DashboardComponent
+  },
+  {
+    path:      'settings',
+    component: SettingsComponent
+  },
+  {
+    path:       '',
+    pathMatch: 'full',
+    redirectTo: '/mood',
+  }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule(
+  {
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  }
+)
+export class AppRoutingModule {
+}
