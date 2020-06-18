49df5dffd860cb2c2c7eb3b3a48670f4fa8aca0e
John Daligault <johndaligault@gmail.com>
2020-06-18 16:48:56 +0200

    adding gradien on mood graph

--- src/app/modules/mood/components/feeling-chart/feeling-chart.component.ts
+++ src/app/modules/mood/components/feeling-chart/feeling-chart.component.ts
@@ -1,6 +1,6 @@
 import {AfterContentInit, Component, Input} from '@angular/core';
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4charts from '@amcharts/amcharts4/charts';
+import {color, create, LinearGradient} from '@amcharts/amcharts4/core';
+import {DateAxis, LineSeries, ValueAxis, XYChart, XYChartScrollbar, XYCursor} from '@amcharts/amcharts4/charts';
 // Importing themes
 import {OverviewModel} from '../../../../../models/overview.model';
 import {OverviewService} from '../../../shared/services/overview.service';
@@ -17,7 +17,7 @@ export class FeelingChartComponent implements AfterContentInit {
 
   @Input() overviews: OverviewModel[];
 
-  private chart: am4charts.XYChart;
+  private chart: XYChart;
 
   constructor(
     private stats: OverviewService,
@@ -27,11 +27,19 @@ export class FeelingChartComponent implements AfterContentInit {
 
   ngAfterContentInit(): void {
     this.instanciateChart();
-    this.chart.data = this.service.getOverviewOfTheMonth(this.overviews);
+    this.chart.data = this
+      .service
+      .getOverviewOfTheMonth(this.overviews)
+      .map(overview => {
+        return {
+          mood:      overview.mood - 5,
+          createdAt: overview.createdAt
+        };
+      });
   }
 
   instanciateChart() {
-    const chart = this.chart = am4core.create('mood-chart', am4charts.XYChart);
+    const chart = this.chart = create('mood-chart', XYChart);
 
     chart.titles.template.fontSize   = 10;
     chart.titles.template.textAlign  = 'end';
@@ -39,55 +47,68 @@ export class FeelingChartComponent implements AfterContentInit {
 
     chart.padding(20, 5, 2, 5);
 
-    const dateAxis                             = chart.xAxes.push(new am4charts.DateAxis());
+    const dateAxis                             = chart.xAxes.push(new DateAxis());
     dateAxis.renderer.grid.template.disabled   = true;
     dateAxis.renderer.labels.template.disabled = true;
     dateAxis.startLocation                     = 0.5;
     dateAxis.endLocation                       = 0.7;
     dateAxis.cursorTooltipEnabled              = false;
 
-    const valueAxis                             = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.min                               = 0;
+    const valueAxis                             = chart.yAxes.push(new ValueAxis());
+    // valueAxis.min                               = 0;
     valueAxis.renderer.grid.template.disabled   = true;
     valueAxis.renderer.baseGrid.disabled        = true;
     valueAxis.renderer.labels.template.disabled = true;
     valueAxis.cursorTooltipEnabled              = false;
 
-    chart.cursor                = new am4charts.XYCursor();
+    chart.cursor                = new XYCursor();
     chart.cursor.lineY.disabled = true;
     chart.cursor.behavior       = 'none';
 
-    const series             = chart.series.push(new am4charts.LineSeries());
+    const series             = chart.series.push(new LineSeries());
     series.dataFields.dateX  = 'createdAt';
     series.dataFields.valueY = 'mood';
     series.tensionX          = 0.8;
     series.strokeWidth       = 2;
 
+    const positiveGradient    = new LinearGradient();
+    positiveGradient.rotation = 90;
 
-// bullet is added because we add tooltip to a bullet for it to change color
-    const bullet       = series.bullets.push(new am4charts.Bullet());
-    bullet.tooltipText = '{valueY}';
+    positiveGradient.addColor(color('#c96264'), 0.8);
+    positiveGradient.addColor(color('#FFFFFF'), 0.8, 0.3);
 
-    bullet.adapter.add('fill', (fill, target) => {
-      // @ts-ignore
-      if (target.dataItem.valueY < 5) {
-        return am4core.color('#cb5154');
-      }
+    series.fillOpacity = 1;
+    series.fill        = positiveGradient;
 
-      return fill;
-    });
+    series.strokeOpacity = 1;
+    series.stroke        = color('#cb5154');
+    series.strokeWidth   = 2;
 
-    const range           = valueAxis.createSeriesRange(series);
-    range.value           = 5;
-    range.endValue        = 0;
-    range.contents.stroke = am4core.color('#cb5154');
-    range.contents.fill   = range.contents.stroke;
+    const range         = valueAxis.createSeriesRange(series);
+    range.contents.fill = range.contents.stroke;
+
+    range.value    = -5;
+    range.endValue = 0;
+
+    const negativeGradient    = new LinearGradient();
+    negativeGradient.rotation = 180 + 90;
+
+    negativeGradient.addColor(color('#575757'), 0.8);
+    negativeGradient.addColor(color('#FFFFFF'), 0.8, 0.3);
+
+    range.contents.fillOpacity = 1;
+    range.contents.fill        = negativeGradient;
+
+    range.contents.strokeOpacity = 1;
+    range.contents.stroke        = color('#2f2f2f');
+    range.contents.strokeWidth   = 2;
+
 
 // Add scrollbar
-    const scrollbarX = new am4charts.XYChartScrollbar();
+    const scrollbarX = new XYChartScrollbar();
     scrollbarX.series.push(series);
     chart.scrollbarX = scrollbarX;
 
-    chart.cursor = new am4charts.XYCursor();
+    chart.cursor = new XYCursor();
   }
 }
